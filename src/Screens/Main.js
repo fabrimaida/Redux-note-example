ab3145638d14406f9898f6c5479599dfdbb7ffcc
fabrimaida <fbrmaida@gmail.com>
2021-04-22 17:00:24 +0200

    Aggiungi testo

--- src/Screens/Main.js
+++ src/Screens/Main.js
@@ -11,6 +11,7 @@ export default class Home extends Component {
                 <Form />
             </div>
             <div className="NotesListHome">
+                <Text>Secondo check branch</Text>
                 <NotesListHome />
             </div>
  
